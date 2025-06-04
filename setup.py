7226ebf034b84c5d519407bee2ac88e77b79e87e
Kyle-Vincent-Bondoc <kylevincentbondoc@gmail.com>
2025-06-05 01:57:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydb",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     description="Database module",
     author="Kyle-Vincent-Bondoc",
